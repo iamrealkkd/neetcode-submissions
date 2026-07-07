7ecf03c2f789de7041d17f2fce0620c941bb7c6b
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-07-07 08:43:04 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int squaresum(int n){
+        int sum = 0;
+        while(n > 0){
+           int dig = n % 10;
+           sum += dig * dig;
+           n /= 10;
+
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do{
+            slow = squaresum(slow);
+            fast = squaresum(squaresum(fast));
+
+
+        }while(slow != fast);
+        return slow == 1;
+    }
+};
